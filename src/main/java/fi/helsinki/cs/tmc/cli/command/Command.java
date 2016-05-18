76fcd8da55c2ae2d8f8a54df73b7bed81e9da976
juvester <juvester@cs.helsinki.fi>
2016-05-18 12:53:51 +0300

    Add some Javadocs

--- src/main/java/fi/helsinki/cs/tmc/cli/command/Command.java
+++ src/main/java/fi/helsinki/cs/tmc/cli/command/Command.java
@@ -4,7 +4,6 @@ package fi.helsinki.cs.tmc.cli.command;
  * Class is an interface for commands.
  */
 public interface Command {
-    
     /**
      * Get command description.
      * @return Description
@@ -21,5 +20,4 @@ public interface Command {
      * Run command.
      */
     void run();
-
 }
--- src/main/java/fi/helsinki/cs/tmc/cli/command/TestCommand.java
+++ src/main/java/fi/helsinki/cs/tmc/cli/command/TestCommand.java
@@ -4,7 +4,6 @@ package fi.helsinki.cs.tmc.cli.command;
  * Class is a test command class
  */
 public class TestCommand implements Command {
-
     @Override
     public String getDescription() {
         return "This is an easter egg test command.";
@@ -19,5 +18,4 @@ public class TestCommand implements Command {
     public void run() {
         System.out.println("Let's run easter egg.");
     }
-    
 }
