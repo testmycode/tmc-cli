f383589694adc29d2b21d1a0707bc77905b11d9d
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-05-19 09:46:00 +0300

    Broken stub implentation for list courses.

--- src/main/java/fi/helsinki/cs/tmc/cli/command/ListCoursesCommand.java
+++ src/main/java/fi/helsinki/cs/tmc/cli/command/ListCoursesCommand.java
@@ -1,5 +1,15 @@
 package fi.helsinki.cs.tmc.cli.command;
 
+import fi.helsinki.cs.tmc.core.TmcCore;
+import fi.helsinki.cs.tmc.core.configuration.TmcSettings;
+import fi.helsinki.cs.tmc.core.domain.Course;
+import fi.helsinki.cs.tmc.core.domain.ProgressObserver;
+import fi.helsinki.cs.tmc.langs.util.TaskExecutor;
+import fi.helsinki.cs.tmc.langs.util.TaskExecutorImpl;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
 /**
  * Class is a test command class
  */
@@ -16,6 +26,14 @@ public class ListCoursesCommand implements Command {
 
     @Override
     public void run() {
-        System.out.println("test.");
+        Callable<List<Course>> callable;
+        TaskExecutor tmcLangs;
+        TmcCore core;
+        String filename = System.getProperty("user.home") + file.separator + ".tmc";
+
+        tmcLangs = new TaskExecutorImpl();
+        core = new TmcCore(settings, tmcLangs);
+        callable = core.listCourses(ProgressObserver.NULL_OBSERVER);
+        List<Course> list = callable.call();
     }
 }
