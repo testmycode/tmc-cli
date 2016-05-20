c6718c66cc595fb0722f04effb101745242bf8d9
jclc <johannes.lakkisto@helsinki.fi>
2016-05-20 11:18:17 +0300

    work in progress

--- src/main/java/fi/helsinki/cs/tmc/cli/tmcstuff/SettingsIo.java
+++ src/main/java/fi/helsinki/cs/tmc/cli/tmcstuff/SettingsIo.java
@@ -9,58 +9,58 @@ import java.io.Reader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Reads and writes to config files on the system.
  */
-public class SettingsIO {
+public class SettingsIo {
 
-    private String getConfigFilePath() {
+    private static final String CONFIGFILE = "tmc.conf";
+
+    public static Path getDefaultConfigRoot() {
         String fileSeparator;
-        String configFileName;
         String configPath;
         String os = System.getProperty("os.name").toLowerCase();
         fileSeparator = System.getProperty("file.separator");
-        configFileName = "tmc.conf";
 
         if (os.contains("windows")) {
             //TODO: Use proper Windows config file location
-            configPath = System.getProperty("user.home") + fileSeparator + configFileName;
+            configPath = System.getProperty("user.home") + fileSeparator;
         } else {
             //Assume we're using Unix (Linux, Mac OS X or *BSD)
             String configEnv = System.getenv("XDG_CONFIG_HOME");
 
             if (configEnv != null && configEnv.length() > 0) {
                 configPath = configEnv
-                        + fileSeparator + "tmccli"
-                        + fileSeparator + configFileName;
+                        + fileSeparator;
             } else {
                 configPath = System.getProperty("user.home")
                         + fileSeparator + ".config"
-                        + fileSeparator + "tmccli"
-                        + fileSeparator + configFileName;
+                        + fileSeparator;
             }
         }
-        return configPath;
-    }
-
-    private Path getConfigFile() {
-
+        configPath = configPath + "tmc-cli" + fileSeparator;
+        return Paths.get(configPath);
     }
 
     public void save(TmcSettings settings) throws IOException {
-        Path location = null;
-        //the above line is temporary!!!
+        String fileSeparator = System.getProperty("file.separator");
+        Path location = settings.getConfigRoot().resolve(fileSeparator + CONFIGFILE);
         Gson gson = new Gson();
         byte[] json = gson.toJson(settings).getBytes();
         Files.write(location, json);
     }
 
-    public TmcSettings load() throws IOException {
-        Path location = null;
-        //the above line is temporary!!!
+    public TmcSettings load(Path configRoot) throws IOException {
+        String fileSeparator = System.getProperty("file.separator");
+        Path location = configRoot.resolve(fileSeparator + CONFIGFILE);
         Gson gson = new Gson();
         Reader reader = Files.newBufferedReader(location, Charset.forName("UTF-8"));
         return gson.fromJson(reader, Settings.class);
+    }
+
+    public TmcSettings load() throws IOException {
+        return load((getDefaultConfigRoot()));
     }
 }
