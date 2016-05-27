821c474fe0bec30362474b22eef0049ed67f5f49
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-05-27 11:39:05 +0300

    Remove unused import.

--- src/test/java/fi/helsinki/cs/tmc/cli/tmcstuff/TmcUtilTest.java
+++ src/test/java/fi/helsinki/cs/tmc/cli/tmcstuff/TmcUtilTest.java
@@ -3,7 +3,6 @@ package fi.helsinki.cs.tmc.cli.tmcstuff;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
 import fi.helsinki.cs.tmc.cli.Application;
 import fi.helsinki.cs.tmc.core.domain.Course;
