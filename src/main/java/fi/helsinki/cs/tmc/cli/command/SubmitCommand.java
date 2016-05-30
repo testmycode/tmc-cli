752c9fd9287938adc75d2e2e44a1020a49871f14
matike <matike@users.noreply.github.com>
2016-05-30 12:57:53 +0300

    Submitting now works in almost every way. Needs cleaning up

--- src/main/java/fi/helsinki/cs/tmc/cli/command/SubmitCommand.java
+++ src/main/java/fi/helsinki/cs/tmc/cli/command/SubmitCommand.java
@@ -1,7 +1,6 @@
 package fi.helsinki.cs.tmc.cli.command;
 
 import fi.helsinki.cs.tmc.cli.Application;
-import fi.helsinki.cs.tmc.cli.io.TmcCliProgressObserver;
 import fi.helsinki.cs.tmc.cli.tmcstuff.DirectoryUtil;
 import fi.helsinki.cs.tmc.cli.tmcstuff.TmcUtil;
 import fi.helsinki.cs.tmc.core.TmcCore;
@@ -9,6 +8,7 @@ import fi.helsinki.cs.tmc.core.domain.Course;
 import fi.helsinki.cs.tmc.core.domain.submission.SubmissionResult;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class SubmitCommand implements Command {
     private Application app;
@@ -31,28 +31,39 @@ public class SubmitCommand implements Command {
     public void run(String[] args) {
         TmcCore core;
         Course course;
-        SubmissionResult submit;
         DirectoryUtil dirUtil;
 
         dirUtil = new DirectoryUtil();
-        core = this.app.getTmcCore();
         Path dir = dirUtil.getCourseDirectory();
 
         if (dir == null) {
-            System.out.println("You are not in course directory");
-            return;
-        }
-        course = TmcUtil.findCourse(core, dir.getName(dir.getNameCount() - 1).toString());
-        String exerciseName = dirUtil.getExerciseName();
-
-        try {
-            submit = core.submit(new TmcCliProgressObserver(),
-                    TmcUtil.findExercise(course, exerciseName)).call();
-
-        } catch (Exception e) {
+            System.out.println("Not a course directory");
             return;
         }
 
-        System.out.println(submit);
+        core = this.app.getTmcCore();
+        String courseName = dir.getName(dir.getNameCount() - 1).toString();
+        course = TmcUtil.findCourse(core, courseName);
+        String exerciseName;
+
+        Path directory = Paths.get(System.getProperty("user.dir"));
+
+        // If no args given, use the exercise directory where the program is run
+        if (args.length > 0) {
+            if (!directory.equals(dirUtil.getCourseDirectory())) {
+                exerciseName = directory.getName(directory.getNameCount() - 1)
+                        .toString() + "-" + args[0];
+            } else {
+                exerciseName = args[0];
+            }
+        } else {
+            exerciseName = dirUtil.getExerciseName();
+        }
+        System.out.println(exerciseName);
+
+        SubmissionResult result;
+        result = TmcUtil.submitExercise(core, course, exerciseName);
+
+        System.out.println(result);
     }
 }
