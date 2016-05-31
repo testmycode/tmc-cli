47aa79ce3b1cffa5371814c127a956122e6a3aed
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-05-31 13:28:22 +0300

    Add the command factory test back.
    It was removed in previous merge. It's name was CommandMapTest in that commit.

---
+++ src/test/java/fi/helsinki/cs/tmc/cli/command/CommandFactoryTest.java
@@ -0,0 +1,40 @@
+package fi.helsinki.cs.tmc.cli.command;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import fi.helsinki.cs.tmc.cli.Application;
+import fi.helsinki.cs.tmc.cli.command.core.CommandFactory;
+import fi.helsinki.cs.tmc.cli.io.TerminalIo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandFactoryTest {
+
+    CommandFactory factory;
+    Application app;
+
+    @Before
+    public void setUp() {
+        factory = new CommandFactory();
+        app = new Application(new TerminalIo());
+    }
+
+    @Test
+    public void constructorAddsCommands() {
+        assertTrue(!factory.getCommands().isEmpty());
+    }
+
+    @Test
+    public void createCommandWorksWithRealCommand() {
+        assertNotNull(factory.createCommand(app, "help"));
+    }
+
+    @Test
+    public void createCommandWorksWithBadCommand() {
+        assertNull(factory.createCommand(app, "foobar"));
+    }
+}
+
