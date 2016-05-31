45ad654b480e1ff4f56753965d6eb637381992ca
juvester <juvester@cs.helsinki.fi>
2016-05-31 14:10:05 +0300

    Add test

---
+++ src/test/java/fi/helsinki/cs/tmc/cli/ShutdownHandlerTest.java
@@ -0,0 +1,29 @@
+package fi.helsinki.cs.tmc.cli;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import fi.helsinki.cs.tmc.cli.io.Io;
+import fi.helsinki.cs.tmc.cli.io.TerminalIo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ShutdownHandlerTest {
+
+    private Io mockIo;
+    private ShutdownHandler shutdownHandler;
+
+    @Before
+    public void setUp() {
+        mockIo = mock(TerminalIo.class);
+        shutdownHandler = new ShutdownHandler(mockIo);
+    }
+
+    @Test
+    public void printsAnsiResetAtRun() {
+        shutdownHandler.run();
+        verify(mockIo).println(eq("\u001B[0m"));
+    }
+}
