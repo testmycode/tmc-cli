17d886ca5cd946516ccfd980a409204997a2da04
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-06-01 12:59:22 +0300

    Add more tests to the command factory.

--- src/test/java/fi/helsinki/cs/tmc/cli/command/CommandFactoryTest.java
+++ src/test/java/fi/helsinki/cs/tmc/cli/command/CommandFactoryTest.java
@@ -3,15 +3,24 @@ package fi.helsinki.cs.tmc.cli.command;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.contains;
 
 import fi.helsinki.cs.tmc.cli.Application;
+import fi.helsinki.cs.tmc.cli.command.core.Command;
 import fi.helsinki.cs.tmc.cli.command.core.CommandFactory;
+import fi.helsinki.cs.tmc.cli.command.core.CommandInterface;
+import fi.helsinki.cs.tmc.cli.io.Io;
 import fi.helsinki.cs.tmc.cli.io.TerminalIo;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CommandFactoryTest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     CommandFactory factory;
     Application app;
@@ -35,6 +44,56 @@ public class CommandFactoryTest {
     @Test
     public void createCommandWorksWithBadCommand() {
         assertNull(factory.createCommand(app, "foobar"));
+    }
+
+    @Command(name = "good", desc = "test")
+    public static class GoodCommand implements CommandInterface {
+        public GoodCommand(Application app) {
+            System.out.println("hello");
+        }
+
+        @Override
+        public void run(String[] args, Io io) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Test
+    public void addGoodCommand() {
+        factory.addCommand(GoodCommand.class);
+        //TODO check the all the stuff in the command
+        assertNotNull(factory.createCommand(app, "good"));
+    }
+
+    public static class BadCommand implements CommandInterface {
+        public BadCommand(Application app) {
+            System.out.println("hello");
+        }
+
+        @Override
+        public void run(String[] args, Io io) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Test
+    public void addCommandWithoutProperAnnotation() {
+        exception.expect(RuntimeException.class);
+        exception.expectMessage(contains("annotation"));
+        factory.addCommand(BadCommand.class);
+    }
+
+    public static class ReallyBadCommand {
+        public ReallyBadCommand(Application app) {
+            System.out.println("hello");
+        }
+    }
+
+    @Test
+    public void addCommandThatDoesntExtendInterface() {
+        exception.expect(RuntimeException.class);
+        exception.expectMessage(contains("Interface"));
+        factory.addCommand(ReallyBadCommand.class);
     }
 }
 
