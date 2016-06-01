802a90953da6f12fc1197c91d32b2dfadc3c2a97
juvester <juvester@cs.helsinki.fi>
2016-06-01 13:50:18 +0300

    Initial implementation for submit results

--- src/main/java/fi/helsinki/cs/tmc/cli/command/SubmitCommand.java
+++ src/main/java/fi/helsinki/cs/tmc/cli/command/SubmitCommand.java
@@ -3,6 +3,7 @@ package fi.helsinki.cs.tmc.cli.command;
 import fi.helsinki.cs.tmc.cli.Application;
 import fi.helsinki.cs.tmc.cli.command.core.Command;
 import fi.helsinki.cs.tmc.cli.command.core.CommandInterface;
+import fi.helsinki.cs.tmc.cli.io.Color;
 import fi.helsinki.cs.tmc.cli.io.Io;
 import fi.helsinki.cs.tmc.cli.io.TmcCliProgressObserver;
 import fi.helsinki.cs.tmc.cli.tmcstuff.DirectoryUtil;
@@ -15,7 +16,9 @@ import java.nio.file.Path;
 
 @Command(name = "submit", desc = "Submit exercises")
 public class SubmitCommand implements CommandInterface {
+
     private Application app;
+    private Io io;
 
     public SubmitCommand(Application app) {
         this.app = app;
@@ -28,6 +31,7 @@ public class SubmitCommand implements CommandInterface {
         SubmissionResult submit;
         DirectoryUtil dirUtil;
 
+        this.io = io;
         dirUtil = new DirectoryUtil();
         core = this.app.getTmcCore();
         Path dir = dirUtil.getCourseDirectory();
@@ -47,6 +51,16 @@ public class SubmitCommand implements CommandInterface {
             return;
         }
 
-        io.println(submit.toString());
+        printResults(submit);
+    }
+
+    private void printResults(SubmissionResult result) {
+        if (result.isAllTestsPassed()) {
+            io.println(Color.colorString("All tests passed!", Color.ANSI_GREEN));
+        } else {
+            io.println(Color.colorString("Tests failed.", Color.ANSI_RED));
+        }
+
+        System.out.println("\n\n\n\n" + result.toString());
     }
 }
