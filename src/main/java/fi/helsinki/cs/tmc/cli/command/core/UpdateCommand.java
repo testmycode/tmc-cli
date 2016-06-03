31bf1ec41c2b24c4fffe60a34e7180bdf27ca93c
matike <matike@users.noreply.github.com>
2016-06-03 13:43:45 +0300

    UpdateCommand created

---
+++ src/main/java/fi/helsinki/cs/tmc/cli/command/core/UpdateCommand.java
@@ -0,0 +1,53 @@
+package fi.helsinki.cs.tmc.cli.command.core;
+
+import fi.helsinki.cs.tmc.cli.Application;
+import fi.helsinki.cs.tmc.cli.io.Io;
+import fi.helsinki.cs.tmc.cli.io.TmcCliProgressObserver;
+import fi.helsinki.cs.tmc.cli.tmcstuff.CourseInfo;
+import fi.helsinki.cs.tmc.cli.tmcstuff.CourseInfoIo;
+import fi.helsinki.cs.tmc.cli.tmcstuff.DirectoryUtil;
+
+import fi.helsinki.cs.tmc.core.TmcCore;
+import fi.helsinki.cs.tmc.core.domain.Exercise;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@Command(name = "update", desc = "Update exercises")
+public class UpdateCommand implements CommandInterface {
+
+    private Application app;
+
+    public UpdateCommand(Application app) {
+        this.app = app;
+    }
+
+    @Override
+    public void run(String[] args, Io io) {
+
+        TmcCore core;
+        Callable<List<Exercise>> callable;
+
+        if (args.length > 0) {
+            io.println("Use in the course directory");
+            return;
+        }
+
+        core = this.app.getTmcCore();
+        if (core == null) {
+            return;
+        }
+        DirectoryUtil dirUtil = new DirectoryUtil();
+        CourseInfoIo infoio = new CourseInfoIo(dirUtil.getConfigFile());
+        CourseInfo info = infoio.load();
+        
+
+        callable = core.getExerciseUpdates(new TmcCliProgressObserver());
+        try {
+            System.out.println(callable.call());;
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+    }
+}
