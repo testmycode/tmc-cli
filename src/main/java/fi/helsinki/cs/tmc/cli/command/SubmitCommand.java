3379c1a9e9647ddb84f9598496097c2f53b46422
mikkomaa <mikko.maatta@helsinki.fi>
2016-06-03 09:58:34 +0300

    Give error message when user tries to submit or run tests in subfolder, and minor refactoring

--- src/main/java/fi/helsinki/cs/tmc/cli/command/SubmitCommand.java
+++ src/main/java/fi/helsinki/cs/tmc/cli/command/SubmitCommand.java
@@ -60,7 +60,6 @@ public class SubmitCommand implements CommandInterface {
         if (core == null) {
             return;
         }
-        Path dir = dirUtil.getCourseDirectory();
         Path courseDir = dirUtil.getCourseDirectory();
 
         if (courseDir == null) {
@@ -75,6 +74,12 @@ public class SubmitCommand implements CommandInterface {
 
         List<String> exercises;
         exercises = dirUtil.getExerciseNames(exerciseNames);
+        
+        if (exercises.isEmpty()) {
+            io.println("You have to be in the exercise root directory to submit."
+                    + " (This is a known problem.)");
+            return;
+        }
 
         ResultPrinter resultPrinter = new ResultPrinter(io, this.showDetails, this.showAll);
         SubmissionResult result;
