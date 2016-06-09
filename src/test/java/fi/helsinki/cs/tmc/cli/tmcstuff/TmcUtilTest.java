4848c28050949da80c31d87ca124a529697d97da
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-06-09 15:16:52 +0300

    Mock the TmcUtil tests.
    Fixes #207.

--- src/test/java/fi/helsinki/cs/tmc/cli/tmcstuff/TmcUtilTest.java
+++ src/test/java/fi/helsinki/cs/tmc/cli/tmcstuff/TmcUtilTest.java
@@ -4,86 +4,148 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import fi.helsinki.cs.tmc.cli.Application;
 import fi.helsinki.cs.tmc.cli.io.TestIo;
+import fi.helsinki.cs.tmc.core.TmcCore;
 import fi.helsinki.cs.tmc.core.domain.Course;
-
 import fi.helsinki.cs.tmc.core.domain.Exercise;
-
-import org.apache.commons.io.FileUtils;
+import fi.helsinki.cs.tmc.core.domain.ProgressObserver;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
 
 
 public class TmcUtilTest {
 
+    static Path workDir;
     Application app;
     TestIo testio;
+    TmcCore mockCore;
 
     @Before
     public void setUp() {
         testio = new TestIo();
         app = new Application(testio);
-        app.createTmcCore(new Settings(true));
-        try {
-            FileUtils.deleteDirectory(new File("cert-test"));
-        } catch (IOException e) {
-        }
+        mockCore = mock(TmcCore.class);
+        app.setTmcCore(mockCore);
+
+        doAnswer(new Answer<Callable<Course>>() {
+            @Override
+            public Callable<Course> answer(InvocationOnMock invocation) throws Throwable {
+                final Course course = (Course)invocation.getArguments()[1];
+                return new Callable<Course>() {
+                    @Override
+                    public Course call() throws Exception {
+                        return course;
+                    }
+                };
+            }
+        }).when(mockCore).getCourseDetails(any(ProgressObserver.class), any(Course.class));
     }
 
     @Test
     public void findCourseIfItExists() {
-        Course course;
-        course = TmcUtil.findCourse(app.getTmcCore(), "demo");
-        assertNotNull(course);
+        final Course course = new Course("right-course");
+        Callable<List<Course>> callable = new Callable<List<Course>>() {
+            @Override
+            public List<Course> call() throws Exception {
+                return Arrays.asList(course, new Course("another"));
+            }
+        };
+        when(mockCore.listCourses(any(ProgressObserver.class))).thenReturn(callable);
+        assertEquals(course, TmcUtil.findCourse(app.getTmcCore(), "right-course"));
     }
 
     @Test
     public void returnNullIfCourseWontExist() {
-        Course course;
-        course = TmcUtil.findCourse(app.getTmcCore(), "afuwhf");
-        assertNull(course);
+        Callable<List<Course>> callable = new Callable<List<Course>>() {
+            @Override
+            public List<Course> call() throws Exception {
+                return Arrays.asList(new Course("course"), new Course("another"));
+            }
+        };
+        when(mockCore.listCourses(any(ProgressObserver.class))).thenReturn(callable);
+        assertNull(TmcUtil.findCourse(app.getTmcCore(), "not-existing-course"));
     }
 
     @Test
-    public void findExerciseIfItExists() {
-        Course course;
-        course = TmcUtil.findCourse(app.getTmcCore(), "demo");
-        assertNotNull(course);
-        Exercise ex = TmcUtil.findExercise(course, "viikko1-002.HeiMaailma");
-        assertNotNull(ex);
-        assertEquals("viikko1-002.HeiMaailma", ex.getName());
+    public void findExerciseOfCourse() {
+        final Course c = new Course("right-course");
+        Exercise exercise = new Exercise("second");
+        c.setExercises(Arrays.asList(new Exercise("first"), exercise));
+        Exercise ex = TmcUtil.findExercise(c, "second");
+        assertEquals(exercise, ex);
     }
 
     @Test
     public void returnNullIfExerciseWontExist() {
-        Course course;
-        course = TmcUtil.findCourse(app.getTmcCore(), "demo");
-        assertNotNull(course);
-        Exercise ex = TmcUtil.findExercise(course, "xhu4ew");
-        assertNull(ex);
+        Course course = new Course("right-course");
+        course.setExercises(Arrays.asList(new Exercise("first"), new Exercise("second")));
+        assertNull(TmcUtil.findExercise(course, "not-existing-exercise"));
     }
 
     @Test
-    public void downloadExercisesWorks() {
-        Course course;
-        String name = "cert-test";
-        course = TmcUtil.findCourse(app.getTmcCore(), name);
-        assertNotNull(course);
-        TmcUtil.downloadAllExercises(app.getTmcCore(), course);
+    public void downloadAllExercises() {
+        String name = "right-course";
+        Course course = new Course(name);
+        course.setExercises(Arrays.asList(new Exercise("first"), new Exercise("second")));
+        course.setExercisesLoaded(true);
 
-        boolean exists = Files.exists(Paths.get(System.getProperty("user.dir")).resolve(name));
-        assertTrue(exists);
-        try {
-            FileUtils.deleteDirectory(new File(name));
-        } catch (IOException e) {
+        doAnswer(new Answer<Callable<List<Exercise>>>() {
+            @Override
+            public Callable<List<Exercise>> answer(InvocationOnMock invocation) throws Throwable {
+                final List<Exercise> exersices = (List<Exercise>)invocation.getArguments()[1];
+                return new Callable<List<Exercise>>() {
+                    @Override
+                    public List<Exercise> call() throws Exception {
+                        return exersices;
+                    }
+                };
+            }
+        }).when(mockCore).downloadOrUpdateExercises(any(ProgressObserver.class), any(List.class));
+        List<Exercise> list = TmcUtil.downloadAllExercises(app.getTmcCore(), course);
+        for (Exercise exercise : list) {
+            assertNotNull(TmcUtil.findExercise(course, exercise.getName()));
+        }
+    }
+
+    @Test
+    public void downloadSomeExercises() {
+        List<Exercise> list = Arrays.asList(new Exercise("first"), new Exercise("second"));
+
+        doAnswer(new Answer<Callable<List<Exercise>>>() {
+            @Override
+            public Callable<List<Exercise>> answer(InvocationOnMock invocation) throws Throwable {
+                final List<Exercise> exersices = (List<Exercise>)invocation.getArguments()[1];
+                return new Callable<List<Exercise>>() {
+                    @Override
+                    public List<Exercise> call() throws Exception {
+                        return exersices;
+                    }
+                };
+            }
+        }).when(mockCore).downloadOrUpdateExercises(any(ProgressObserver.class), any(List.class));
+        List<Exercise> downloadList = TmcUtil.downloadExercises(app.getTmcCore(), list);
+        for (Exercise exercise : downloadList) {
+            boolean found = false;
+            for (Exercise exercise2 : list) {
+                if (exercise.getName().equals(exercise2.getName())) {
+                    found = true;
+                }
+            }
+            assertTrue(found);
         }
     }
 }
