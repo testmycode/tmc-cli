3071d3f5d2d9f7da80dc508d88e646a1bcfb0672
juvester <juvester@cs.helsinki.fi>
2016-06-09 14:48:03 +0300

    Add test stub

---
+++ src/test/java/fi/helsinki/cs/tmc/cli/command/SubmitCommandTest.java
@@ -0,0 +1,36 @@
+package fi.helsinki.cs.tmc.cli.command;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SubmitCommandTest {
+
+    public SubmitCommandTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void stub() {
+        assertTrue(true);
+    }
+}
