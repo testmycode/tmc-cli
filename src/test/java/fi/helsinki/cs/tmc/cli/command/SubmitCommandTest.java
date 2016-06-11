be13f5045155440d81f04b7a5bc631ea3af1de7e
juvester <juvester@cs.helsinki.fi>
2016-06-11 20:13:11 +0300

    Prepare dummy course

--- src/test/java/fi/helsinki/cs/tmc/cli/command/SubmitCommandTest.java
+++ src/test/java/fi/helsinki/cs/tmc/cli/command/SubmitCommandTest.java
@@ -1,5 +1,6 @@
 package fi.helsinki.cs.tmc.cli.command;
 
+import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
@@ -8,13 +9,34 @@ import fi.helsinki.cs.tmc.cli.io.TestIo;
 import fi.helsinki.cs.tmc.core.TmcCore;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SubmitCommandTest {
+
+    static Path pathToDummyCourse;
+    static Path pathToDummyExercise;
 
     Application app;
     TestIo io;
     TmcCore mockCore;
+
+    @BeforeClass
+    public static void setUpClass() {
+        pathToDummyCourse = Paths.get(SubmitCommandTest.class.getClassLoader()
+                .getResource("dummy-courses/2016-aalto-c")
+                .getPath());
+
+        pathToDummyExercise = Paths.get(SubmitCommandTest.class.getClassLoader()
+                .getResource("dummy-courses/2016-aalto-c/Module_1-02_intro")
+                .getPath());
+
+        assertNotNull(pathToDummyCourse);
+        assertNotNull(pathToDummyExercise);
+    }
 
     @Before
     public void setUp() {
