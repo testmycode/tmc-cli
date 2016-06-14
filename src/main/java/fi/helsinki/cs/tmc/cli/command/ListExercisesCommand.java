6ad81e32788c5a9572e460a110b69949bdb4bc9a
matike <matike@users.noreply.github.com>
2016-06-14 10:29:49 +0300

    Ifs fixed

--- src/main/java/fi/helsinki/cs/tmc/cli/command/ListExercisesCommand.java
+++ src/main/java/fi/helsinki/cs/tmc/cli/command/ListExercisesCommand.java
@@ -87,12 +87,17 @@ public class ListExercisesCommand extends AbstractCommand {
         for (Exercise exercise : exercises) {
             // Check the exercise status in order of flag importance, for example there's
             // no need to check if deadline has passed if the exercise has been submitted
-            if (exercise.isAllReviewPointsGiven()) {
+            if (exercise.isAllReviewPointsGiven() && exercise.requiresReview()) {
                 sb.append(Color.colorString(
                         "Completed: ", Color.ANSI_GREEN) + exercise.getName() + "\n");
             } else if (exercise.isCompleted()) {
-                sb.append(Color.colorString(
-                        "Requires review: ", Color.ANSI_YELLOW) + exercise.getName() + "\n");
+                if (exercise.requiresReview()) {
+                    sb.append(Color.colorString(
+                            "Requires review: ", Color.ANSI_YELLOW) + exercise.getName() + "\n");
+                } else {
+                    sb.append(Color.colorString(
+                            "Completed: ", Color.ANSI_GREEN) + exercise.getName() + "\n");
+                }
             } else if (exercise.hasDeadlinePassed()) {
                 sb.append(Color.colorString(
                         "Deadline passed: ", Color.ANSI_PURPLE) + exercise.getName() + "\n");
