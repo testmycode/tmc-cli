7d1f7fd3638c8856f9f0313e6757c866f0b13296
juvester <juvester@cs.helsinki.fi>
2016-06-15 02:57:41 +0300

    Add annotations

--- src/main/java/fi/helsinki/cs/tmc/cli/command/LogoutCommand.java
+++ src/main/java/fi/helsinki/cs/tmc/cli/command/LogoutCommand.java
@@ -1,12 +1,14 @@
 package fi.helsinki.cs.tmc.cli.command;
 
 import fi.helsinki.cs.tmc.cli.command.core.AbstractCommand;
+import fi.helsinki.cs.tmc.cli.command.core.Command;
 import fi.helsinki.cs.tmc.cli.io.Io;
 import fi.helsinki.cs.tmc.cli.tmcstuff.SettingsIo;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 
+@Command(name = "logout", desc = "Logout from TMC server")
 public class LogoutCommand extends AbstractCommand {
 
     @Override
--- src/test/java/fi/helsinki/cs/tmc/cli/command/LogoutCommandTest.java
+++ src/test/java/fi/helsinki/cs/tmc/cli/command/LogoutCommandTest.java
@@ -5,6 +5,7 @@ import static org.junit.matchers.JUnitMatchers.containsString;
 import static org.mockito.Mockito.times;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
+import fi.helsinki.cs.tmc.cli.Application;
 import fi.helsinki.cs.tmc.cli.io.TestIo;
 import fi.helsinki.cs.tmc.cli.tmcstuff.SettingsIo;
 
@@ -19,19 +20,21 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @PrepareForTest(SettingsIo.class)
 public class LogoutCommandTest {
 
+    Application app;
     TestIo io;
 
     @Before
     public void setUp() {
         io = new TestIo();
+        app = new Application(io);
 
         PowerMockito.mockStatic(SettingsIo.class);
     }
 
     @Test
     public void hello() {
-        LogoutCommand logoutCommand = new LogoutCommand();
-        logoutCommand.run(null, io);
+        String[] args = {"logout"};
+        app.run(args);
 
         verifyStatic(times(1));
         SettingsIo.delete();
