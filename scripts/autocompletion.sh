94b9963dc24d17313da3804c2f3044a8113d8734
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-06-17 14:49:21 +0300

    Autocomplete filenames.

--- scripts/autocompletion.sh
+++ scripts/autocompletion.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-set -euo pipefail
-
 # SCRIPT_PATH will be filled by stub script
 alias tmc="$SCRIPT_PATH/tmc"
 
-tmcCommands=\$(tmc shell-helper -c)
+TMC_COMMANDS=( "\$(tmc shell-helper -c)" )
 
 _tmc_opts()
 {
-  local cur
-  # Pointer to current completion word.
-  # By convention, it's named "cur" but this isn't strictly necessary.
+	local cur
+	local sub_command
+	local main_args
+	# Pointer to current completion word.
+	# By convention, it's named "cur" but this isn't strictly necessary.
 
-  COMPREPLY=()
-  cur="\${COMP_WORDS[COMP_CWORD]}"
+	COMPREPLY=()
+	cur="\${COMP_WORDS[COMP_CWORD]}"
 
-  case "\$cur" in
-    -*)
-      COMPREPLY=( \$( compgen -W "-v -h --version --help" -- "\$cur" ) )
-      return 0
-      ;;
-#   xx) May add more cases here.
-#   yy)
-#   zz)
-  esac
+	main_args=( "\${COMP_WORDS[@]:0:COMP_CWORD}" )
+	for word in "\${main_args[@]}"; do
+		if [[ "\$TMC_COMMANDS" =~ "\$word" ]]; then
+			sub_command="\$word"
+		fi
+	done
 
-  COMPREPLY=( \$( compgen -W "\$tmcCommands" -- "\$cur" ) )
-  return 0
+	case "\$cur" in
+	    -*)
+		COMPREPLY=( \$( compgen -W "-v -h --version --help" -- "\$cur" ) )
+		return 0
+		;;
+	esac
+
+	if [[ ! -z "\$sub_command" ]]; then
+		COMPREPLY=( \$(compgen -f -- \${cur}) )
+		return 0
+	fi
+
+	COMPREPLY=( \$( compgen -W "\${TMC_COMMANDS}" -- "\$cur" ) )
+	return 0
 }
 
 complete -F _tmc_opts tmc
