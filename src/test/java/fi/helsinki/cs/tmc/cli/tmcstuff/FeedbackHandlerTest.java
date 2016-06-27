395c3b67eece8e85fe7d05de663263e78c25f956
jclc <johannes.lakkisto@helsinki.fi>
2016-06-27 11:25:24 +0300

    setup method

--- src/test/java/fi/helsinki/cs/tmc/cli/tmcstuff/FeedbackHandlerTest.java
+++ src/test/java/fi/helsinki/cs/tmc/cli/tmcstuff/FeedbackHandlerTest.java
@@ -2,6 +2,7 @@ package fi.helsinki.cs.tmc.cli.tmcstuff;
 
 import fi.helsinki.cs.tmc.cli.io.ExternalsUtil;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -10,4 +11,8 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @PrepareForTest(ExternalsUtil.class)
 public class FeedbackHandlerTest {
 
+    @Before
+    public void setup() {
+        
+    }
 }
