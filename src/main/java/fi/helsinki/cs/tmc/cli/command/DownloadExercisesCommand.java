12067eb3ebf28abb168bd778e5c8a39667217e0f
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-07-01 13:17:20 +0300

    Fix import warnings.

--- src/main/java/fi/helsinki/cs/tmc/cli/command/DownloadExercisesCommand.java
+++ src/main/java/fi/helsinki/cs/tmc/cli/command/DownloadExercisesCommand.java
@@ -20,7 +20,6 @@ import org.apache.commons.cli.Options;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
--- src/test/java/fi/helsinki/cs/tmc/cli/command/DownloadExercisesCommandTest.java
+++ src/test/java/fi/helsinki/cs/tmc/cli/command/DownloadExercisesCommandTest.java
@@ -32,8 +32,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.powermock.reflect.Whitebox;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.reflect.Whitebox;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
